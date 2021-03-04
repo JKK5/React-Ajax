8d0283c5890016f899e5a7a9665b661c2f1bedf3
JKK5 <jshane@g.skku.edu>
2021-03-04 22:02:43 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="1">HTML</a>
+          </li>
+          <li>
+            <a href="1">CSS</a>
+          </li>
+          <li>
+            <a href="1">JS</a>
+          </li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello. React & Ajax
+      </article>
+    </div>
+  );
+}
+
+export default App;
